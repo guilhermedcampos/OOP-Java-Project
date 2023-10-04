27d8f3824490e3c35dd487cdf68137e8171d79ed
Guilherme Campos <guilhermedcampos7@gmail.com>
2023-10-04 17:03:42 +0100

    .

---
+++ xxl-skeleton/xxl/core/Range.java
@@ -0,0 +1,16 @@
+public class Range {
+    private Cell _startCell;
+    private Cell _endCell;
+
+    public Cell getStartCell() {
+        return _startCell;
+    }
+
+    public Cell getEndCell() {
+        return _endCell;
+    }
+
+    public Cell[] traverse() {
+        
+    }
+}
