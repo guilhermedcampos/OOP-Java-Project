0f9cb8f13595e4f46daf2559ecf57b34fde1fa50
Guilherme Campos <guilhermedcampos7@gmail.com>
2023-10-09 21:01:08 +0100

    DoSave

--- xxl-skeleton/xxl/app/main/DoSave.java
+++ xxl-skeleton/xxl/app/main/DoSave.java
@@ -1,12 +1,15 @@
 package xxl.app.main;
 
 import pt.tecnico.uilib.forms.Form;
-import pt.tecnico.uilib.menus.Command;
-import xxl.core.Calculator;
-// FIXME import classes
+import pt.tecnico.uilib.forms.Field;
+import xxl.app.main.Message;
+import xxl.core.Spreadsheet;
+import java.io.Serial;
+import java.io.Serializable;
+import java.io.IOException;
 
 /**
- * Save to file under current name (if unnamed, query for name).
+ * Save to a file under the current name (if unnamed, query for name).
  */
 class DoSave extends Command<Calculator> {
 
@@ -16,6 +19,36 @@ class DoSave extends Command<Calculator> {
   
   @Override
   protected final void execute() {
-    // FIXME implement command and create a local Form
+      Spreadsheet spreadsheet = receiver.getSpreadsheet();
+
+      if (spreadsheet == null) {
+          display(Message.newSaveAs()); // Prompt for a name if there's no spreadsheet
+          return;
+      }
+
+      String fileName = null;
+      if (spreadsheet.getName() == null) {
+          Form form = new Form("Save Spreadsheet");
+          Field<String> fileNameField = form.addStringField("fileName", "Enter file name: ");
+          form.parse();
+
+          // Get the file name entered by the user
+          fileName = fileNameField.value();
+      } else {
+          fileName = spreadsheet.getName();
+      }
+
+      try {
+          // Serialize and save the spreadsheet with the specified file name in the current directory
+          try (FileOutputStream fileOut = new FileOutputStream(fileName + ".ser");
+              ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+              objectOut.writeObject(spreadsheet);
+              display("Spreadsheet saved to: " + fileName + ".ser");
+          } catch (IOException e) {
+              display(Message.problemOpeningFile(e)); // Use problemOpeningFile message for IO exceptions
+          }
+      } catch (Exception e) {
+          display("Error: " + e.getMessage());
+      }
   }
 }
