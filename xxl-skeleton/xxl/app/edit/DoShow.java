acb34ec4c47cf6c066aa56757c380b7e47cdaf1e
Guilherme Campos <guilhermedcampos7@gmail.com>
2023-10-11 17:57:39 +0100

    .

--- xxl-skeleton/xxl/app/edit/DoShow.java
+++ xxl-skeleton/xxl/app/edit/DoShow.java
@@ -21,6 +21,10 @@ class DoShow extends Command<Spreadsheet> {
   protected final void execute() throws CommandException {
     Spreadsheet spreadsheet = Calculator.getSpreadsheet();
     Cell[][] cells = spreadsheet.getCells();
+    System.out.println("oi");
+    for (Cell cell : cells) {
+      System.out.println(cell.toString()+"|"+cell.getContent().toString());
+    }
 
   }
 }
