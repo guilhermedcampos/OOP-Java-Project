671d0c54703186187c2071fb1d62bceb1edbe4a9
Maria Ramos <maria.m.ramos@tecnico.ulisboa.pt>
2023-10-11 21:25:39 +0100

    oi

--- xxl-skeleton/xxl/app/main/DoSave.java
+++ xxl-skeleton/xxl/app/main/DoSave.java
@@ -1,11 +1,9 @@
 package xxl.app.main;
 
 import pt.tecnico.uilib.forms.Form;
-import pt.tecnico.uilib.forms.Field;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.Display;
 import pt.tecnico.uilib.menus.CommandException;
-
 import xxl.core.Calculator;
 import xxl.core.Spreadsheet;
 import xxl.app.main.Message;
@@ -16,46 +14,58 @@ import java.io.IOException;
 import java.io.ObjectOutputStream;
 
 /**
- * Save to a file under the current name (if unnamed, query for name).
+ * Command to save the current state to a file under the current name or prompt
+ * for a name if unnamed.
  */
 class DoSave extends Command<Calculator> {
 
-  DoSave(Calculator receiver) {
-    super(Label.SAVE, receiver);
-  }
-  
-  @Override
-  protected final void execute() {
-      Spreadsheet spreadsheet = _receiver.getSpreadsheet();
-      Form form = new Form("Save File");
+    /**
+     * Constructs a new instance of the DoSave command.
+     *
+     * @param receiver The calculator instance to which this command is attached.
+     */
+    DoSave(Calculator receiver) {
+        super(Label.SAVE, receiver);
+    }
 
-      /*
-      if (spreadsheet == null) {
-          _display.popup(Message.newSaveAs()); 
-          return;
-      }
-      */
+    /**
+ * Executes the "Save" command, which saves the current state to a file under
+ * the current name or prompts for a name if unnamed.
+ */
+    @Override
+    protected final void execute() {
+        // Get the current spreadsheet from the calculator
+        Spreadsheet spreadsheet = _receiver.getSpreadsheet();
 
-      String fileName = spreadsheet.getName(); // Initialize it with spreadsheet name
+        // Create a form for saving
+        Form form = new Form("Save File");
 
-      if (fileName == null) {
-          form.addStringField("fileName", Message.newSaveAs());
-          form.parse();
-          fileName = form.stringField("fileName");
-          spreadsheet.setName(fileName);
-      } 
+        // Attempt to retrieve the file name associated with the spreadsheet
+        String fileName = spreadsheet.getName();
 
-      try {
-          // Serialize and save the spreadsheet with the specified file name in the current directory
-          try (FileOutputStream fileOut = new FileOutputStream(fileName + ".ser");
-              ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
-              objectOut.writeObject(spreadsheet);
-          } catch (IOException e) {
-              _display.popup(Message.problemOpeningFile(e)); // Use problemOpeningFile message for IO exceptions
-          }
-      } catch (Exception e) {
-          _display.addLine("Error: " + e.getMessage());
-          _display.display();
-      }
-  }
+        // If the file name is not set, prompt the user to provide a file name
+        if (fileName == null) {
+            form.addStringField("fileName", Message.newSaveAs());
+            form.parse();
+            fileName = form.stringField("fileName");
+
+            // Set the spreadsheet's name to the provided file name
+            spreadsheet.setName(fileName);
+        }
+
+        try {
+            // Serialize and save the spreadsheet to a file with the specified name
+            try (FileOutputStream fileOut = new FileOutputStream(fileName + ".ser");
+                    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+                objectOut.writeObject(spreadsheet);
+            } catch (IOException e) {
+                // Display an error message if there is a problem saving the file
+                _display.popup(Message.problemOpeningFile(e));
+            }
+        } catch (Exception e) {
+            // Display a generic error message in case of any other exceptions
+            _display.addLine("Error: " + e.getMessage());
+            _display.display();
+        }
+    }
 }
